3d1bb2dba24c067d9cf93cd1efcae5baf3934040
Amara002 <amaraalbalkhi@yahoo.com>
2021-08-12 23:03:03 +0300

    finish lab26

---
+++ app/src/main/java/com/example/taskmasteragain/AllTask.java
@@ -0,0 +1,16 @@
+package com.example.taskmasteragain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_task);
+    }
+
+
+}
